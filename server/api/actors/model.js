6a8d58f27163fe05b83a81be7b6e529556e191e9
Girma Nigusse <xgirma@gmail.com>
2016-08-02 22:49:06 -0400

    add actors/:id/movies

--- server/api/actors/model.js
+++ server/api/actors/model.js
@@ -3,11 +3,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var MoviesSchema = new Schema({
+  title: String,
+  year: Number
+});
+
 var ActorsSchema = new Schema({
   name: {
     first: String,
     last: String
-  }
+  },
+  bio: String,
+  movies : [MoviesSchema]
 });
 
 module.exports = mongoose.model('actors', ActorsSchema);
